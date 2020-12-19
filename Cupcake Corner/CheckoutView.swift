e8b1ef68c51b6c43cff205aa582b47d41736b77f
Midhet Sulemani <midhetfatema94@gmail.com>
2020-12-19 16:11:13 +0300

    added networking APIs

---
+++ Cupcake Corner/CheckoutView.swift
@@ -0,0 +1,75 @@
+//
+//  CheckoutView.swift
+//  Cupcake Corner
+//
+//  Created by Waveline Media on 12/17/20.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
+    @ObservedObject var order: Order
+
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView {
+                VStack {
+                    Image("cupcakes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width)
+
+                    Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+                        .font(.title)
+
+                    Button("Place Order") {
+                        self.placeOrder()
+                    }
+                    .padding()
+                }
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
+        .alert(isPresented: $showingConfirmation) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        }
+    }
+    
+    func placeOrder() {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode data")
+            return
+        }
+        
+        let urlString = "https://reqres.in/api/cupcakes"
+        let url = URL(string: urlString)!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            guard let data = data else {
+                print("No data in response: ", error?.localizedDescription ?? "Unknown error")
+                return
+            }
+            
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+                self.showingConfirmation = true
+            } else {
+                print("Invalid response from server")
+            }
+        }.resume()
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
--- Cupcake Corner/ContentView.swift
+++ Cupcake Corner/ContentView.swift
@@ -18,47 +18,51 @@ struct Result: Codable {
 }
 
 struct ContentView: View {
-    @State private var results = [Result]()
+    @ObservedObject var order = Order()
     
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0 ..< Order.types.count) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper(value: $order.quantity, in: 3 ... 20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                        Text("Any special requests?")
+                    }
+
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting) {
+                            Text("Add extra frosting")
+                        }
+
+                        Toggle(isOn: $order.addSprinkles) {
+                            Text("Add extra sprinkles")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: AddressView(order: order)) {
+                        Text("Delivery details")
+                    }
+                }
             }
+            .navigationBarTitle("Cupcake Corner")
         }
-        .onAppear(perform: loadData)
     }
     
     func loadData() {
-        let urlString = "https://itunes.apple.com/search?term=taylor+swift&entity=song"
         
-        guard let url = URL(string: urlString) else {
-            print("Invalid URL")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    // we have good data – go back to the main thread
-                    DispatchQueue.main.async {
-                        // update our UI
-                        self.results = decodedResponse.results
-                    }
-
-                    // everything is good, so we can exit
-                    return
-                }
-            }
-
-            // if we're still here it means there was a problem
-            print("Fetch failed: ", error?.localizedDescription ?? "Unknown error")
-            
-        }.resume()
     }
 }
 
